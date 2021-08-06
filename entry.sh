3e2aabdd51969855b8043d9eca1a70ef96baa3b0
Tyson L. Swetnam <tswetnam@cyverse.org>
2021-08-06 16:01:13 +0000

    updated irods_environment.json again

--- entry.sh
+++ entry.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo '{"irods_host": "data.cyverse.org", "irods_port": 1247, "irods_user_name": "$IPLANT_USER", "irods_zone_name": "iplant"}' | tee  > /home/user/.irods/irods_environment.json
+echo '{"irods_host": "data.cyverse.org", "irods_port": 1247, "irods_zone_name": "iplant"}' | tee  > /home/user/.irods/irods_environment.json
 
 /usr/bin/tini -- ttyd bash
