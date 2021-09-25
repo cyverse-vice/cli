20698e1abdca4f9b73a4df220f54b6ac4e54e000
Tyson L. Swetnam <tyson-swetnam@users.noreply.github.com>
2021-09-25 13:47:39 -0700

    Update entry.sh

--- zsh/entry.sh
+++ zsh/entry.sh
@@ -1,7 +1,5 @@
 #!/bin/zsh
 
-source ~/.zshrc
-
 echo '{"irods_host": "data.cyverse.org", "irods_port": 1247, "irods_zone_name": "iplant"}' | tee  > /home/user/.irods/irods_environment.json
 
 exec /usr/bin/tini -- ttyd tmux new -A -s ttyd zsh 
