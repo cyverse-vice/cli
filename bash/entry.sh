b77393481fb9e40146717edddfc420d27d35390e
Tyson L. Swetnam <tyson-swetnam@users.noreply.github.com>
2022-01-27 09:16:29 -0700

    Update entry.sh

--- bash/entry.sh
+++ bash/entry.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo '{"irods_host": "data.cyverse.org", "irods_port": 1247, "irods_zone_name": "iplant"}' | tee  > /home/user/.irods/irods_environment.json
+echo '{"irods_host": "data.cyverse.org", "irods_port": 1247, "irods_user_name": "$IPLANT_USER", "irods_zone_name": "iplant"}' | envsubst > $HOME/.irods/irods_environment.json
 
 exec /usr/bin/tini -- ttyd tmux new -A -s ttyd bash
