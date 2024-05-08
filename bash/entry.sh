68d500e1bbe519c52f27398b6fbc0d08a3cec8b8
Tyson L. Swetnam <tyson-swetnam@users.noreply.github.com>
2024-05-08 12:00:48 -0700

    Update entry.sh

--- bash/entry.sh
+++ bash/entry.sh
@@ -2,4 +2,9 @@
 
 echo '{"irods_host": "data.cyverse.org", "irods_port": 1247, "irods_user_name": "$IPLANT_USER", "irods_zone_name": "iplant"}' | envsubst > $HOME/.irods/irods_environment.json
 
+# Copy .gitconfig from volume mount (if it exists)
+if [ -f ~/data-store/iplant/home/{$IPLANT_USER}/.gitconfig ]; then
+  cp ~/data-store/iplant/home/{$IPLANT_USER}/.gitconfig ~/.gitconfig
+fi
+
 exec /usr/bin/tini -- ttyd tmux new -A -s ttyd bash
