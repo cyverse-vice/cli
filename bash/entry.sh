77534c2ede01dfe116ee61e218ec3cd1bc4608fb
Tyson L. Swetnam <tyson-swetnam@users.noreply.github.com>
2025-09-18 13:42:39 -0700

    Copy AWS and SSH config directories if present

--- bash/entry.sh
+++ bash/entry.sh
@@ -4,7 +4,15 @@ echo '{"irods_host": "data.cyverse.org", "irods_port": 1247, "irods_user_name": 
 
 # Copy .gitconfig from volume mount (if it exists)
 if [ -f /data-store/iplant/home/$IPLANT_USER/.gitconfig ]; then
-  cp /data-store/iplant/home/$IPLANT_USER/.gitconfig ~/
+  cp /data-store/iplant/home/$IPLANT_USER/.gitconfig ~/ 
+fi
+
+if [ -d /data-store/iplant/home/$IPLANT_USER/.aws ]; then
+  cp -r /data-store/iplant/home/$IPLANT_USER/.aws ~/ 
+fi
+
+if [ -d /data-store/iplant/home/$IPLANT_USER/.ssh ]; then
+  cp -r /data-store/iplant/home/$IPLANT_USER/.ssh ~/ 
 fi
 
 exec /usr/bin/tini -- ttyd tmux new -A -s ttyd bash
