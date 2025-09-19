aeea830150bf36ba715890cdcda0cdf458f30a92
Tyson L. Swetnam <tyson-swetnam@users.noreply.github.com>
2025-09-19 07:03:34 -0700

    Add iRODS and MCP server configuration
    Added configuration for iRODS and MCP server.

--- bash/entry.sh
+++ bash/entry.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
-
+# add irods config
 echo '{"irods_host": "data.cyverse.org", "irods_port": 1247, "irods_user_name": "$IPLANT_USER", "irods_zone_name": "iplant"}' | envsubst > $HOME/.irods/irods_environment.json
+# add mcp server
+echo '{"mcpServers":{"filesystem":{"command":"npx","args":["-y","@modelcontextprotocol/server-filesystem","/home/jovyan/work","/home/jovyan/data","/home/jovyan/notebooks","/home/jovyan"],"env":{}}}}' > /home/jovyan/.claude.json
 
 # Copy .gitconfig from volume mount (if it exists)
 if [ -f /data-store/iplant/home/$IPLANT_USER/.gitconfig ]; then
   cp /data-store/iplant/home/$IPLANT_USER/.gitconfig ~/ 
 fi
 
+# Copy S3 AWS (if it exists)
 if [ -d /data-store/iplant/home/$IPLANT_USER/.aws ]; then
   cp -r /data-store/iplant/home/$IPLANT_USER/.aws ~/ 
 fi
 
+# Copy SSH keys (if it exists)
 if [ -d /data-store/iplant/home/$IPLANT_USER/.ssh ]; then
   cp -r /data-store/iplant/home/$IPLANT_USER/.ssh ~/ 
 fi
